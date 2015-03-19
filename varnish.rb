fb76e3bf97c8856db9bc4d5252615e4af9c03eed
Jonathan Colby <jcolby@team.mobile.de>
2015-03-19 15:49:36 +0100

    varnish class

---
+++ varnish.rb
@@ -0,0 +1,33 @@
+class Varnish
+  
+  ### load yaml config
+  
+  ### set defaults
+  
+  ### variables
+  
+  # secret key
+  # varnish version
+  # admin port
+  
+  ### methods
+  
+  # status
+  
+  # banner
+  
+  # varnishadm
+  
+  # list backends
+  
+  # ban list
+  
+  # ban all (purge)
+  
+  # backend enable/disable
+  
+  # params show
+  
+ 
+  
+end
