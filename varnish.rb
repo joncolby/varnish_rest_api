5db9875d53cadb299bf71af22b8a5f3515bc59aa
Jonathan Colby <jcolby@team.mobile.de>
2015-03-20 15:26:21 +0100

    work-in-progress

--- varnish.rb
+++ varnish.rb
@@ -1,4 +1,15 @@
+
+require 'open3'
+
 class Varnish
+  
+    
+  
+  def initialize(instance="default")
+    @instance = instance
+    @binary = '/Users/jcolby/projects/varnish-rest-api/varnishadm'
+
+  end
   
   ### load yaml config
   
@@ -12,7 +23,7 @@ class Varnish
   
   ### methods
   
-  # status
+
   
   # banner
   
@@ -28,6 +39,36 @@ class Varnish
   
   # params show
   
- 
+  # status
+  def status
+    varnishadm("status")  
+  end
+  
+  def varnishadm(cmd)
+    #stdin, stdout, stderr = Open3.popen3(@binary + " " + cmd)
+    #puts "std_out: " + stdout.readlines.join('-')
+    #puts "std_err: " + stderr.readlines.join('-')
+    
+    Open3.popen3(@binary + ' ' + cmd) do |stdin, stdout, stderr, wait_thr|
+      puts "std_out: " + stdout.readlines.join('-')
+      puts "std_err: " + stderr.readlines.join('-')
+      
+      exit_status = wait_thr.value
+      unless exit_status.success?
+        abort "FAILED !!! #{cmd}"
+      end
+    end
+    
+  end
+  
+  def to_s
+    "instance #{@instance}"
+  end
+  
+  private :varnishadm
   
 end
+
+v = Varnish.new
+
+v.status
